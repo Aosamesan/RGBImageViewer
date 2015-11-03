18b85dff09b000a0fc011d32702beceb3911d895
Aosamesan <bluerein_@naver.com>
2015-11-03 20:28:39 +0900

    처음 커밋

---
+++ RGBImageViewer/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace RGBImageViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofDlg = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열기ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(926, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 열기ToolStripMenuItem
+            // 
+            this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+            this.열기ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.열기ToolStripMenuItem.Text = "열기";
+            this.열기ToolStripMenuItem.Click += new System.EventHandler(this.열기ToolStripMenuItem_Click);
+            // 
+            // ofDlg
+            // 
+            this.ofDlg.Filter = "RGB File|*.rgb";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(926, 789);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 813);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "RGB ImageViewer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog ofDlg;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ RGBImageViewer/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RGBImageViewer
+{
+    public partial class Form1 : Form
+    {
+        RGBImage image;
+        public Form1()
+        {
+            InitializeComponent();
+            image = new RGBImage();
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(ofDlg.ShowDialog() == DialogResult.OK)
+            {
+                image.OpenImage(ofDlg.FileName);
+                pictureBox.Image = image.GetImage(1920);
+            }
+        }
+    }
+
+    public class RGBImage
+    {
+        public string FileName { get; set; }
+        public byte[] RedChannel { get; private set; }
+        public byte[] GreenChannel { get; private set; }
+        public byte[] BlueChannel { get; private set; }
+        public int FileSize { get; private set; }
+        public int ImageWidth { get; private set; }
+        public int ImageHeight { get; private set; }
+
+        public void OpenImage(string fileName)
+        {
+            FileName = fileName;
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                int idx = 0;
+                FileSize = Convert.ToInt32(fs.Length);
+                int channelLength = FileSize / 3;
+                RedChannel = new byte[channelLength];
+                GreenChannel = new byte[channelLength];
+                BlueChannel = new byte[channelLength];
+                byte b;
+                for(idx = 0; idx < FileSize; idx++)
+                {
+                    b = Convert.ToByte(fs.ReadByte());
+                    switch(idx % 3)
+                    {
+                        case 0:
+                            RedChannel[idx / 3] = b;
+                            break;
+                        case 1:
+                            GreenChannel[idx / 3] = b;
+                            break;
+                        case 2:
+                            BlueChannel[idx / 3] = b;
+                            break;
+                    }
+                }
+            }
+            ImageWidth = ImageHeight = 0;
+        }
+
+        public Image GetImage(int width)
+        {
+            ImageWidth = width;
+            ImageHeight = FileSize / ImageWidth / 3;
+            Image image = new Bitmap(ImageWidth, ImageHeight);
+            Pen p;
+            Color c;
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                for(int i = 0; i < ImageHeight; i++)
+                {
+                    for(int j = 0; j < ImageWidth; j++)
+                    {
+                        c = Color.FromArgb(RedChannel[j + i * ImageWidth], GreenChannel[j + i * ImageWidth], BlueChannel[j + i * ImageWidth]);
+                        p = new Pen(c);
+                        g.DrawRectangle(p, j, i, 1, 1);
+                        p.Dispose();
+                    }
+                }
+            }
+
+            return image;
+        }
+    }
+}
